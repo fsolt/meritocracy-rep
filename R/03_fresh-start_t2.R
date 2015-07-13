cc125f937cd3f3f39362d61b8f6055504b8a0649
Frederick Solt <fsolt@mac.com>
2015-07-13 14:28:39 -0500

    try to get plots with library(coefplot)

--- R/03_fresh-start_t2.R
+++ R/03_fresh-start_t2.R
@@ -74,6 +74,16 @@ format_mi_results <- function(m) {
     m_vars2 <- list()
     m_vars2 <- lapply(m_vars, as.matrix)
     m_res <- MIcombine(m_fe, m_vars2)
+    b <- m_res$coefficients
+    se <- diag(m_res$variance^.5)
+    data.frame(Value = b,
+               Coefficient = names(b),
+               HighInner = b, # no inner CI, but variable is still needed by coefplot
+               LowInner = b,
+               HighOuter = b+qnorm(.975)*se,
+               LowOuter = b-qnorm(.975)*se,
+               Model = deparse(substitute(m)), 
+               stringsAsFactors = FALSE)
 }
 
 cnty_data <- read_csv("data/cnty_data.csv")
@@ -165,6 +175,38 @@ for (i in 1:length(yrs)) {
                         (1|fips), family=binomial(link="logit")))
     t2_by_surv[[i]] <- format_mi_results(res)
 }
-lapply(t2_by_surv, summary)
+lapply(t2_by_surv, print)
 beep()
 
+coef_plot <- function(df, coef, lower, upper, varnames, groupname = NULL, xlab, label.pos = c(0, 1, 2), fontsize = NULL, monochrome = NULL)
+{
+    # This function creates a dot-and-whisker plot for regression coefficients, first differences, etc.
+    # It requires the following arguments:
+    # coef: a (numeric) vector of coefficients/first differences/...
+    ## If using groups of variables: enter NA for the empty coefficient designating a group
+    # se: a vector of standard errors
+    ## If using groups of variables: enter NA for the empty SE designating a group
+    # varnames: a (character) vector of variable names
+    ## If using groups of variables: enter the group name where you want it to appear
+    # groupname: an optional (character) vector of variable groups with the same length as varnames
+    # xlab: a character vector of length 1 with the label for the x-axis
+    # label.pos: Position of the x-axis tick labels; 0 (default) for left, 0.5 for center, 1 for right
+    # monochrome: 0 for colors, 1 for black dots and whiskers
+    # Optional arguments:
+    # fontsize: an optional numeric vector of varying font sizes (if using groups)
+    
+    var <- seq(length(varnames), 1, by = -1)
+    group <- as.numeric(as.factor(groupname))
+    results <- data.frame(var=var, varnames=varnames, coef=coef, lower = lower, upper = upper, group=group, groupname=groupname)
+  
+    ggplot(results, aes(x = coef, y = as.factor(var))) +
+        geom_point(colour = "black") + 
+        geom_segment(aes(x = lower, xend = upper, y = var, yend = var), colour = "black") +
+        theme_bw() + xlab(paste(xlab)) + ylab("") + 
+        scale_y_discrete(breaks=results$var, labels=results$varnames) + 
+        geom_vline(xintercept = 0, colour = "blue", linetype = 2) + 
+        theme(legend.position = "none", 
+              axis.text.y = element_text(colour = "black", hjust = label.pos),
+              axis.text.x = element_text(colour = "black"))
+
+}
