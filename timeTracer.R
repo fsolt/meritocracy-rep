fe1b0c247585e949d692385bfa8ed814d7ccf805
sammo3182 <yue-hu-1@uiowa.edu>
2015-11-09 11:15:22 -0600

    Time tracer, let R to record your spending time

---
+++ timeTracer.R
@@ -0,0 +1,20 @@
+#############Set up the first line ################################
+# Time tracing
+trace <- data.frame(start = NA, end = NA, spent = NA)
+trace$start <- Sys.time() - 3600
+trace$end <- Sys.time()
+trace$spent <- difftime(trace$end,trace$start, units = "hours")
+###################################################################
+
+# run when at the starting point
+trace_updated <- data.frame(start = NA, end = NA, spent = NA)
+trace_updated$start <- Sys.time() 
+
+# run this at the ending point
+trace_updated$end <- Sys.time() 
+trace_updated$spent <- difftime(trace_updated$end,trace_updated$start, units = "hours")
+
+trace <- rbind(trace, trace_updated)
+
+sum(trace$spent)
+
