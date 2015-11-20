492f37ff631bb6fcba38b4737a3e1a1deba5d7e8
fsolt <fsolt@mac.com>
2015-11-19 21:14:22 -0600

    finish getting mean for all versions

--- R/04_use_consistent_measures.R
+++ R/04_use_consistent_measures.R
@@ -1,54 +1,65 @@
-library(stringr)
 library(rio)
+library(stringr)
+library(dplyr)
 
 data_path <- "data/pew/merit/"
 ver_a_files <- list.files(data_path, recursive = TRUE) %>% 
-	str_subset("version_a.*sav|dta")	#misses typo1999c which is .dat in fwf
+	str_subset("version_a.*sav|dta")
 ver_a <- data.frame(
-	file_name = paste0(data_path, ver_a_files[c(1:2, 5:9, 11:13, 16)]),
-	year = c(2011, 2012, 2007, 2005, 2011.1, 2006, 2014, 2014.1,
-			 2010, 2000, 2004), stringsAsFactors = FALSE) %>% arrange(year)
+	file_name = paste0(data_path, ver_a_files[c(1:2, 5:8, 12:14, 17)]),
+	year = c(2011, 2012, 2007, 2005, 2011, 2006,
+			 2010, 2000, 1999, 2004), stringsAsFactors = FALSE) %>% arrange(year)
 rc2 <- "1 = 0; 2 = 1; else = NA"
 rc4 <- "c(1,2) = 0; c(3,4) = 1; else = NA"
 
 ver_a <- ver_a %>% mutate(
-	var_name = c("Q18K", "q11k", "q14k", "q8b", "q5c", "q30d", "q17k", 
-				 "q44a", "q11", "q44a", "q25k"),
-	weight = c("WEIGHT", rep("weight", 10)),
-	rc = c(rep(rc4, 3), rep(rc2, 3), rc4, rep(rc2, 4)),
-	version = "Version A",
+	var_name = c("q18j", "Q18K", "q11k", "q14k", "q8b", "q5c", "q30d", "q17k", 
+				 "q44a", "q11"),
+	weight = c("weight", "WEIGHT", rep("weight", 8)),
+	rc = c(rep(rc4, 4), rep(rc2, 3), rc4, rep(rc2, 2)),
+	version = "A",
 	value = NA)
 
-get_mean <- function(x) {
+ver_a1 <- ver_a %>% filter(year!=2011)
+ver_a2 <- ver_a %>% filter(year==2011)
+
+setup <- function(x) {
 	df <- import(x$file_name)
 	df$rej_merit <- car::recode(as.numeric(df[[x$var_name]]), x$rc)
-	weighted.mean(df$rej_merit, df[[x$weight]], na.rm = TRUE) *100 %>% round(1)
+	df$weight <- df[[x$weight]]
+	df <- df %>% select(rej_merit, weight) %>% mutate(year = x$year)
+	return(df)
 }
 	
-for (i in 1:nrow(ver_a)) {
-	ver_a$value[i] <- get_mean(ver_a[i, ])
-# 	x <- ver_a[i, ]
-# 	df <- import(x$file_name)
-# 	df$rej_merit <- car::recode(as.numeric(df[[x$var_name]]), x$rc)
-# 	ver_a$value[i] <- Hmisc::wtd.mean(df$rej_merit, df[[x$weight]]) * 100 %>% round()
-#	ver_a$se[i] <- (Hmisc::wtd.var(df$rej_merit, df[[x$weight]], normwt = TRUE) %>% sqrt()) / sqrt(length(length(df$rej_merit[!is.na(df$rej_merit)])))
+for (i in 1:nrow(ver_a1)) {
+	df <- setup(ver_a1[i, ])
+	ver_a1$value[i] <- weighted.mean(df$rej_merit, df$weight, na.rm = TRUE) *100
 }
 
-dt_va[["1999"]] <- data.frame(value = recode(dt99$q18k, "c(1,2) = 0; c(3,4) = 1; else = NA"), weight = dt99$weight)
+df <- rbind(setup(ver_a2[1, ]), setup(ver_a2[2, ]))
+ver_a2 <- data.frame(
+	year = 2011, 
+	version = "A", 
+	value = weighted.mean(df$rej_merit, df$weight, na.rm = TRUE) *100,
+	stringsAsFactors = FALSE)
 
-dt_va[["2000"]] <- data.frame(value = recode(dt00$Q18K, "c('Statement #1 -  strongly','Statement #1 - not strongly') = 0; c('Statement #2 -  strongly', 'Statement #2 -  not strongly') = 1; else = NA") %>% as.numeric() - 1, weight = dt00$WEIGHT)
-dt_va[["2004"]] <- data.frame(value = recode(dt04$q11k, "c(1,2) = 0; c(3,4) = 1; else = NA"), weight = dt04$weight)
-dt_va[["2005"]] <- data.frame(value = as.numeric(dt05$q14k) %>% recode("c(1,2) = 0; c(3,4) = 1; else = NA"), weight = dt05$weight)
-dt_va[["2006"]] <- data.frame(value = as.numeric(dt06$q8b) %>% recode("1 = 0; 2 = 1; else = NA"), weight = dt06$weight)
+ver_b_file <- import(paste0(data_path, "version_b/1987-2012 Values Merge/1987-2012 Values Merge public.sav")) 
+ver_b_file$rej_merit <- with(ver_b_file, ifelse(q2f < 3 & q2e < 3, 1, 0))
+ver_b_file$rej_merit[ver_b_file$q2f == 9 | ver_b_file$q2e == 9] <- NA
+ver_b <- ver_b_file %>% group_by(year) %>% 
+	summarize(version = "B",
+			  value = weighted.mean(rej_merit, weight, na.rm = TRUE) * 100) %>% 
+	filter(year>=1999)
 
-dt_va[["2010"]] <- data.frame(value = as.numeric(dt10$q30d) %>% recode("1 = 0; 2 = 1; else = NA"), weight = dt10$weight)
-dt_va[["2011"]] <- data.frame(value = as.numeric(dt11$q44a) %>% recode("1 = 0; 2 = 1; else = NA"), weight = dt11$weight)
-dt_va[["2011_2"]] <- data.frame(value = as.numeric(dt11_2$q17k) %>% recode("c(1,2) = 0; c(3,4) = 1; else = NA"), weight = dt11_2$weight)
-dt_va[["2012"]] <- data.frame(value = as.numeric(dt12$q11) %>% recode("1 = 0; 2 = 1; else = NA"), weight = dt12$weight)
-dt_va[["2014"]] <- data.frame(value = as.numeric(dt14$q44a) %>% recode("1 = 0; 2 = 1; else = NA"), weight = dt14$weight)
-dt_va[["2014_2"]] <- data.frame(value = as.numeric(dt14_2$q25k) %>% recode("1 = 0; 2 = 1; else = NA"), weight = dt14_2$weight)
+ver_c_file <- ver_b_file 
+ver_c_file$rej_merit <- with(ver_c_file, ifelse(q2f < 3, 1, 0))
+ver_c_file$rej_merit[ver_c_file$q2f == 9] <- NA
+ver_c <- ver_c_file %>% group_by(year) %>% 
+	summarize(version = "C",
+			  value = weighted.mean(rej_merit, weight, na.rm = TRUE) * 100) %>% 
+	filter(year>=1999)
+
+ver_all <- ver_a1 %>% select(year, version, value) %>% rbind(ver_a2, ver_b, ver_c)
+ver_all_wide <- ver_all %>% tidyr::spread(key = version, value = value)
 
 
-version_a <- list()
-
-for (i in 1:length)
